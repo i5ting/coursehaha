d93104b8a3b788805578385f2ac0dc7249a7153c
i5ting <i5ting@126.com>
2017-10-25 06:16:00 +0800

    lazycommmit, please ignore

--- test/test.js
+++ test/test.js
@@ -1,8 +1,13 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
-    });
-  });
-});
+var chai = require('chai')
+var assert = chai.assert // Using Assert style
+var expect = chai.expect // Using Expect style
+var should = chai.should() // Using Should style
+
+describe('Array', function () {
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            //   assert.equal(-1, [1,2,3].indexOf(4))
+            expect(-1).to.equal([1, 2, 3].indexOf(4))
+        })
+    })
+})

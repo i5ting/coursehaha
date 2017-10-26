dbc569f80a2d16a1432bc61adaca12db5d7e3c25
i5ting <i5ting@126.com>
2017-10-27 07:02:42 +0800

    lazycommmit, please ignore

--- db_find_test.js
+++ db_find_test.js
@@ -2,8 +2,14 @@ var db = require('./db')
 var Course = require('./app/model/course')
 // console.log(Course)
 
+// db.once('open', function () {
+//     var all = Course.all(function (err, users) {
+//         console.log(users)
+//     })
+// })
+
 db.once('open', function () {
-    var all = Course.all(function (err, users) {
+    var course = Course.getById('59efc22cca69d2122ce75823',function (err, users) {
         console.log(users)
     })
 })
--- routes/api/courses.js
+++ routes/api/courses.js
@@ -5,15 +5,15 @@ var Course = require('../../app/model/course')
 // console.log(Course)
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var all = Course.all(function (err, users) {
-    console.log(users)
+router.get('/', function (req, res, next) {
+  Course.all(function (err, courses) {
+    console.log(courses)
     res.json({
       data: {
-        users: users
+        courses: courses
       },
       status: {
-        code:0,
+        code: 0,
         msg: "sucess"
       }
     })
@@ -21,17 +21,46 @@ router.get('/', function(req, res, next) {
 });
 
 /* GET user detail. */
-router.get('/:id', function(req, res, next) {
-  res.json({
-    a:1
-  });
+router.get('/:id', function (req, res, next) {
+  var user_id = req.params.id
+  Course.getById(user_id, function (err, course) {
+    console.log(course)
+    res.json({
+      data: {
+        course: course
+      },
+      status: {
+        code: 0,
+        msg: "sucess"
+      }
+    })
+  })
 });
 
 /* POST create user */
-router.post('/', function(req, res, next) {
-  res.json({
-    a:1
-  });
+router.post('/', function (req, res, next) {
+  Course.create({
+      name: req.body.name || "系统掌握深度学习，从入门到精通实战", //课程名称
+      category: req.body.category || "/首页/人工智能/系统掌握深度学习",
+      price:req.body.price ||  2999, //价格
+      picture: req.body.picture || "https://new.stuq.org/files/default/2017/08-08/120213522984724939.jpg" ,//头图
+      status: req.body.status || "更新中", //状态标签：更新中、直播课程、不可用
+      plan: req.body.plan || "教学计划：系统掌握深度学习，从...", //教学计划：系统掌握深度学习，从...
+      effect_duration:req.body.effect_duration ||  "学习有效期：365 天（随到随学）", //学习有效期：365 天（随到随学）
+      prompt: req.body.prompt || "课程原价 4999 元，现在限名额优惠，报名请尽快扫码咨询课程小助手", //课程提示： 课程原价 4999 元...
+      target:req.body.target ||  "让学员掌握机器学习、深度学习基本原理", //课程目标
+      target_user:req.body.target_user ||  "有一定编程基础，对深度学习有浓厚兴趣", //适合人群
+  },function(err, course){
+    res.json({
+      data: {
+        course: course
+      },
+      status: {
+        code: 0,
+        msg: "sucess"
+      }
+    })
+  })
 });
 
 module.exports = router;

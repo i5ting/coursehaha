090b37c998cfc5b90dea8449afae059602f77210
i5ting <i5ting@126.com>
2017-10-29 09:08:26 +0800

    lazycommmit, please ignore

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var Course = require('../app/model/course');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  Course.all(function (err, courses) {
+    console.log(courses)
+    res.render('index', { 
+      title: 'Express',
+      courses:courses
+    });
+  })
 });
 
 module.exports = router;

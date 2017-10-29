bee3a537611876acb0aa8b8137a319e0085c1276
i5ting <i5ting@126.com>
2017-10-29 11:06:00 +0800

    lazycommmit, please ignore

--- routes/index.js
+++ routes/index.js
@@ -5,16 +5,35 @@ var Course = require('../app/model/course');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  Course.all(function (err, courses) {
+  var num = req.query.num ? req.query.num : 2
+
+  Course.top(num, function (err, courses) {
+    if (courses.length % 2 === 1){
+      // 移除最后一个
+      courses.pop()
+    }
     console.log(courses)
-    res.render('index', { 
+    res.render('index_v2', { 
       title: 'Express',
+      show_mode: true,
       courses:courses
     });
   })
 });
 
+/* GET list page. */
+router.get('/list', function (req, res, next) {
+  Course.all(function (err, courses) {
+    console.log(courses)
+    res.render('index_v2', { 
+      title: 'Express',
+      show_mode: false,
+      courses:courses
+    });
+  })
+});
 
+/* GET static list page. */
 router.get('/static/list', function (req, res, next) {
   Course.all(function (err, courses) {
     console.log(courses)
